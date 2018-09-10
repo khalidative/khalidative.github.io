222f628b2f48d9458671ba146e89ea4148b9e310
Khalid Mohamed <mohamedkhalid149@hotmail.com>
2018-09-10 10:05:04 +0530

    Add files via upload form home-linuxmint

---
+++ js/main.js
@@ -0,0 +1,169 @@
+jQuery(document).ready(function( $ ) {
+
+  // Smooth scroll for the menu and links with .scrollto classes
+  $('.smothscroll').on('click', function(e) {
+    e.preventDefault();
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      if (target.length) {
+
+        $('html, body').animate({
+          scrollTop: target.offset().top - 62
+        }, 1500, 'easeInOutExpo');
+      }
+    }
+  });
+
+  $('.carousel').carousel({
+    interval: 3500
+  });
+
+  // C# Chart
+  var doughnutData = [{
+      value: 90,
+      color: "#1abc9c"
+    },
+    {
+      value: 10,
+      color: "#ecf0f1"
+    }
+  ];
+  var myDoughnut = new Chart(document.getElementById("C#").getContext("2d")).Doughnut(doughnutData);
+
+  // Nodejs Chart
+  var doughnutData = [{
+    value: 80,
+    color: "#1abc9c"
+  },
+  {
+    value: 20,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("Nodejs").getContext("2d")).Doughnut(doughnutData);
+  
+  // C++ Chart
+  var doughnutData = [{
+    value: 80,
+    color: "#1abc9c"
+  },
+  {
+    value: 20,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("C++").getContext("2d")).Doughnut(doughnutData);
+
+  // Python Chart
+  var doughnutData = [{
+    value: 70,
+    color: "#1abc9c"
+  },
+  {
+    value: 30,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("Python").getContext("2d")).Doughnut(doughnutData);
+
+  // Java Chart
+  var doughnutData = [{
+    value: 70,
+    color: "#1abc9c"
+  },
+  {
+    value: 30,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("Java").getContext("2d")).Doughnut(doughnutData);
+
+  // C Chart
+  var doughnutData = [{
+    value: 90,
+    color: "#1abc9c"
+  },
+  {
+    value: 10,
+    color: "#ecf0f1"
+  }
+  ];
+   var myDoughnut = new Chart(document.getElementById("C").getContext("2d")).Doughnut(doughnutData);
+
+ /* // Scala Chart
+  var doughnutData = [{
+    value: 60,
+    color: "#1abc9c"
+  },
+  {
+    value: 40,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("Scala").getContext("2d")).Doughnut(doughnutData);
+
+  // Dart Chart
+  var doughnutData = [{
+    value: 60,
+    color: "#1abc9c"
+  },
+  {
+    value: 40,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("Dart").getContext("2d")).Doughnut(doughnutData);
+  
+  // F# Chart
+  var doughnutData = [{
+    value: 50,
+    color: "#1abc9c"
+  },
+  {
+    value: 50,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("F#").getContext("2d")).Doughnut(doughnutData);
+
+*/
+
+// SQLServer Chart
+  var doughnutData = [{
+    value: 80,
+    color: "#1abc9c"
+  },
+  {
+    value: 20,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("SQLServer").getContext("2d")).Doughnut(doughnutData);
+
+  // MongoDB Chart
+  var doughnutData = [{
+    value: 70,
+    color: "#1abc9c"
+  },
+  {
+    value: 30,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("MongoDB").getContext("2d")).Doughnut(doughnutData);
+  
+  // F# Chart
+  var doughnutData = [{
+    value: 60,
+    color: "#1abc9c"
+  },
+  {
+    value: 40,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("PostgreSQL").getContext("2d")).Doughnut(doughnutData);
+
+  
+  
+});
